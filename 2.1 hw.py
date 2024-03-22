fe48a399e53ba4435c9d101abf3ce075d3d9ca76
sayansgithub <161965997+sayansgithub@users.noreply.github.com>
2024-03-22 18:23:35 -0400

    Add files via upload

---
+++ 2.1 hw.py
@@ -0,0 +1,5 @@
+
+
+## i want to learn to code so i can make games and do projects.
+print('i want to learn how to make websites and games')
+print('to do math\nfor fun\nCoding Improves Problem Solving Skills\nmore job opportunities\nto understand Technology')
