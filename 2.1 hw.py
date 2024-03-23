4159d18d935bffb1732cff1e6eccda6a3abeca15
sayansgithub <161965997+sayansgithub@users.noreply.github.com>
2024-03-23 12:58:31 -0400

    Update 2.1 hw.py

--- 2.1 hw.py
+++ 2.1 hw.py
@@ -1,5 +1,21 @@
-
-
 ## i want to learn to code so i can make games and do projects.
+
 print('i want to learn how to make websites and games')
 print('to do math\nfor fun\nCoding Improves Problem Solving Skills\nmore job opportunities\nto understand Technology')
+print("FALLING UP\t\tby Shel Silverstein  \nI tripped on my shoelace\nAnd I fell up\nUp to the roof tops,\n\tUp over the town,\n\t\tUp past the tree tops,\n\t\t\tUp over the mountains,\n\t\t\t\tUp where the colours,\n\t\t\t\t\tBlend into the sounds.\n\t\t\t\tBut it got me so dizzy\n\t\t\tWhen I looked around,\n\t\tI got sick to my stomach\n\tAnd I threw down")
+
+##print("sayan")
+##print("Each new print statement is printed on a new line!")
+##print("We have extra characters that have special meaning too!")
+##print("\tbackslash t prints out a tab\twhich is like several spaces")
+##print("backslash n\nprints a new line\neach time you type it\n!")
+
+print('sayan jindal\ns\na\ny\na\nn\n\n j\ni\nn\nd\na\nl\n\n')
+          
+print('"2024-03-02"')
+
+print('sunday\nmonday\ntuseday\nWednesday\nThursday \nfriday\nsaturday')
+
+print('\t1\t2\t3\t4\t5\t6\t7\t8')
+
+print('HOW MANY, HOW MUCH \t\tby Shel SilversteinHow many slams in an old screen door?Depends how loud you shut it.How many slices in a bread?Depends how thin you cut it.How much good inside a day?Depends \nhow good you live em.\n\tHow much love inside a friend?\nDepends how much you give em.')
